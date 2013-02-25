877340f28474aa90f85fe0b672f76c44aac1d5ad
Sean Conner <spc@conman.org>
2013-02-25 04:43:23 -0500

    Bug fix---compiler error due to new changes.
    Remove extraneous variable initialization due to recent changes.

--- src/globals.c
+++ src/globals.c
@@ -101,7 +101,7 @@ volatile bool  gf_debug       = false;
 Blog           g_blog;
 struct display gd =
 {
-  { false , false , false , false , true , true , false , false} ,
+  { false , false , false , true , true , false , false} ,
   INDEX
 };
 
