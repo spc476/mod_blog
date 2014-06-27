b61a9975c79d00131f84bc15b8a1dd9a27659e1c
Sean Conner <spc@conman.org>
2014-06-27 03:02:24 -0400

    Support more filetypes for mime_type().
    And because of that change, we go from a linear search to a binary search
    based on the extension.

--- src/backend.c
+++ src/backend.c
@@ -59,7 +59,7 @@
 
 static void	   calculate_previous		(struct btm);
 static void	   calculate_next		(struct btm);
-static const char *mime_type			(char *);
+static const char *mime_type			(const char *);
 static int	   display_file			(FILE *,Tumbler);
 static char       *tag_collect			(List *);
 static char	  *tag_pick                     (const char *);
@@ -551,34 +551,195 @@ static void calculate_next(struct btm end)
 
 /******************************************************************/
 
-static const char *mime_type(char *filename)
+static int dstring_cmp(const void *needle,const void *haystack)
 {
-  int                         i;
-  static const struct dstring types[] = 
+  const char           *key   = needle;
+  const struct dstring *value = haystack;
+  
+  return strcmp(key,value->s1);
+}
+
+static const char *mime_type(const char *filename)
+{
+  static const struct dstring  types[] = 
   {
-    { ".jpeg"		, "image/jpeg"	},
-    { ".jpg"		, "image/jpeg"	},
-    { ".gif"		, "image/gif"	},
-    { ".png"		, "image/png"	},
-    { ".x-html"		, "text/x-html" },
-    { ".html"		, "text/html"   },
-    { ".htm"            , "text/html"   },
-    { ".txt"		, "text/plain"	},
-    { ".css"		, "text/css"	},
-    { ".mp4"		, "video/mp4"	},
-    { ".mov"		, "video/quicktime" },
-    { ".gz"		, "application/x-gzip" },
-    { NULL		, NULL		}
+    { ".ai"		, "application/postscript"		} ,
+    { ".aif"		, "audio/x-aiff"			} ,
+    { ".aifc"		, "audio/x-aiff"			} ,
+    { ".aiff"		, "audio/x-aiff"			} ,
+    { ".asc"		, "text/plain"				} ,
+    { ".au"		, "audio/basic"				} ,
+    { ".avi"		, "video/x-msvideo"			} ,
+    { ".bcpio"		, "application/x-bcpio"			} ,
+    { ".bin"		, "application/octet-stream"		} ,
+    { ".bmp"		, "image/bmp"				} ,
+    { ".cdf"		, "application/x-netcdf"		} ,
+    { ".cgm"		, "image/cgm"				} ,
+    { ".class"		, "application/octet-stream"		} ,
+    { ".cpio"		, "application/x-cpio"			} ,
+    { ".cpt"		, "application/mac-compactpro"		} ,
+    { ".csh"		, "application/x-csh"			} ,
+    { ".css"		, "text/css"				} ,
+    { ".dcr"		, "application/x-director"		} ,
+    { ".dir"		, "application/x-director"		} ,
+    { ".djv"		, "image/vnd.djvu"			} ,
+    { ".djvu"		, "image/vnd.djvu"			} ,
+    { ".dll"		, "application/octet-stream"		} ,
+    { ".dms"		, "application/octet-stream"		} ,
+    { ".doc"		, "application/msword"			} ,
+    { ".dtd"		, "application/xml-dtd"			} ,
+    { ".dvi"		, "application/x-dvi"			} ,
+    { ".dxr"		, "application/x-director"		} ,
+    { ".eps"		, "application/postscript"		} ,
+    { ".etx"		, "text/x-setext"			} ,
+    { ".exe"		, "application/octet-stream"		} ,
+    { ".ez"		, "application/andrew-inset"		} ,
+    { ".gif"		, "image/gif"				} ,
+    { ".gram"		, "application/srgs"			} ,
+    { ".grxml"		, "application/srgs+xml"		} ,
+    { ".gtar"		, "application/x-gtar"			} ,
+    { ".gz"		, "application/x-gzip"			} ,
+    { ".hdf"		, "application/x-hdf"			} ,
+    { ".hqx"		, "application/mac-binhex40"		} ,
+    { ".htm"		, "text/html"				} ,
+    { ".html"		, "text/html"				} ,
+    { ".ice"		, "x-conference/x-cooltalk"		} ,
+    { ".ico"		, "image/x-icon"			} ,
+    { ".ics"		, "text/calendar"			} ,
+    { ".ief"		, "image/ief"				} ,
+    { ".ifb"		, "text/calendar"			} ,
+    { ".iges"		, "model/iges"				} ,
+    { ".igs"		, "model/iges"				} ,
+    { ".jpe"		, "image/jpeg"				} ,
+    { ".jpeg"		, "image/jpeg"				} ,
+    { ".jpg"		, "image/jpeg"				} ,
+    { ".js"		, "application/x-javascript"		} ,
+    { ".kar"		, "audio/midi"				} ,
+    { ".latex"		, "application/x-latex"			} ,
+    { ".lha"		, "application/octet-stream"		} ,
+    { ".lzh"		, "application/octet-stream"		} ,
+    { ".m3u"		, "audio/x-mpegurl"			} ,
+    { ".man"		, "application/x-troff-man"		} ,
+    { ".mathml"		, "application/mathml+xml"		} ,
+    { ".me"		, "application/x-troff-me"		} ,
+    { ".mesh"		, "model/mesh"				} ,
+    { ".mid"		, "audio/midi"				} ,
+    { ".midi"		, "audio/midi"				} ,
+    { ".mif"		, "application/vnd.mif"			} ,
+    { ".mov"		, "video/quicktime"			} ,
+    { ".movie"		, "video/x-sgi-movie"			} ,
+    { ".mp2"		, "audio/mpeg"				} ,
+    { ".mp3"		, "audio/mpeg"				} ,
+    { ".mp4"		, "video/mp4"				} ,
+    { ".mpe"		, "video/mpeg"				} ,
+    { ".mpeg"		, "video/mpeg"				} ,
+    { ".mpg"		, "video/mpeg"				} ,
+    { ".mpga"		, "audio/mpeg"				} ,
+    { ".ms"		, "application/x-troff-ms"		} ,
+    { ".msh"		, "model/mesh"				} ,
+    { ".mxu"		, "video/vnd.mpegurl"			} ,
+    { ".nc"		, "application/x-netcdf"		} ,
+    { ".oda"		, "application/oda"			} ,
+    { ".ogg"		, "application/ogg"			} ,
+    { ".pbm"		, "image/x-portable-bitmap"		} ,
+    { ".pdb"		, "chemical/x-pdb"			} ,
+    { ".pdf"		, "application/pdf"			} ,
+    { ".pgm"		, "image/x-portable-graymap"		} ,
+    { ".pgn"		, "application/x-chess-pgn"		} ,
+    { ".png"		, "image/png"				} ,
+    { ".pnm"		, "image/x-portable-anymap"		} ,
+    { ".ppm"		, "image/x-portable-pixmap"		} ,
+    { ".ppt"		, "application/vnd.ms-powerpoint"	} ,
+    { ".ps"		, "application/postscript"		} ,
+    { ".qt"		, "video/quicktime"			} ,
+    { ".ra"		, "audio/x-realaudio"			} ,
+    { ".ram"		, "audio/x-pn-realaudio"		} ,
+    { ".ras"		, "image/x-cmu-raster"			} ,
+    { ".rdf"		, "application/rdf+xml"			} ,
+    { ".rgb"		, "image/x-rgb"				} ,
+    { ".rm"		, "audio/x-pn-realaudio"		} ,
+    { ".roff"		, "application/x-troff"			} ,
+    { ".rpm"		, "audio/x-pn-realaudio-plugin"		} ,
+    { ".rtf"		, "text/rtf"				} ,
+    { ".rtx"		, "text/richtext"			} ,
+    { ".sgm"		, "text/sgml"				} ,
+    { ".sgml"		, "text/sgml"				} ,
+    { ".sh"		, "application/x-sh"			} ,
+    { ".shar"		, "application/x-shar"			} ,
+    { ".silo"		, "model/mesh"				} ,
+    { ".sit"		, "application/x-stuffit"		} ,
+    { ".skd"		, "application/x-koan"			} ,
+    { ".skm"		, "application/x-koan"			} ,
+    { ".skp"		, "application/x-koan"			} ,
+    { ".skt"		, "application/x-koan"			} ,
+    { ".smi"		, "application/smil"			} ,
+    { ".smil"		, "application/smil"			} ,
+    { ".snd"		, "audio/basic"				} ,
+    { ".so"		, "application/octet-stream"		} ,
+    { ".spl"		, "application/x-futuresplash"		} ,
+    { ".src"		, "application/x-wais-source"		} ,
+    { ".sv4cpio"	, "application/x-sv4cpio"		} ,
+    { ".sv4crc"		, "application/x-sv4crc"		} ,
+    { ".svg"		, "image/svg+xml"			} ,
+    { ".swf"		, "application/x-shockwave-flash"	} ,
+    { ".t"		, "application/x-troff"			} ,
+    { ".tar"		, "application/x-tar"			} ,
+    { ".tcl"		, "application/x-tcl"			} ,
+    { ".tex"		, "application/x-tex"			} ,
+    { ".texi"		, "application/x-texinfo"		} ,
+    { ".texinfo"	, "application/x-texinfo"		} ,
+    { ".tif"		, "image/tiff"				} ,
+    { ".tiff"		, "image/tiff"				} ,
+    { ".tr"		, "application/x-troff"			} ,
+    { ".tsv"		, "text/tab-separated-values"		} ,
+    { ".txt"		, "text/plain"				} ,
+    { ".ustar"		, "application/x-ustar"			} ,
+    { ".vcd"		, "application/x-cdlink"		} ,
+    { ".vrml"		, "model/vrml"				} ,
+    { ".vxml"		, "application/voicexml+xml"		} ,
+    { ".wav"		, "audio/x-wav"				} ,
+    { ".wbmp"		, "image/vnd.wap.wbmp"			} ,
+    { ".wbxml"		, "application/vnd.wap.wbxml"		} ,
+    { ".wml"		, "text/vnd.wap.wml"			} ,
+    { ".wmlc"		, "application/vnd.wap.wmlc"		} ,
+    { ".wmls"		, "text/vnd.wap.wmlscript"		} ,
+    { ".wmlsc"		, "application/vnd.wap.wmlscriptc"	} ,
+    { ".wrl"		, "model/vrml"				} ,
+    { ".x-html"		, "text/x-html" 			} ,
+    { ".xbm"		, "image/x-xbitmap"			} ,
+    { ".xht"		, "application/xhtml+xml"		} ,
+    { ".xhtml"		, "application/xhtml+xml"		} ,
+    { ".xls"		, "application/vnd.ms-excel"		} ,
+    { ".xml"		, "application/xml"			} ,
+    { ".xpm"		, "image/x-xpixmap"			} ,
+    { ".xsl"		, "application/xml"			} ,
+    { ".xslt"		, "application/xslt+xml"		} ,
+    { ".xul"		, "application/vnd.mozilla.xul+xml"	} ,
+    { ".xwd"		, "image/x-xwindowdump"			} ,
+    { ".xyz"		, "chemical/x-xyz"			} ,
+    { ".zip"		, "application/zip"			} ,
   };
+  
+  const struct dstring *v;
+  const char           *ext;
   
   assert(filename != NULL);
   
-  for (i = 0 ; types[i].s1 != NULL ; i++)
+  ext = strrchr(filename,'.');
+
+  if (ext != NULL)
   {
-    if (strstr(filename,types[i].s1) != NULL)
-      return(types[i].s2);
+    v = bsearch(
+                 ext,
+                 types,
+                 sizeof(types)/sizeof(struct dstring),
+                 sizeof(struct dstring),
+                 dstring_cmp
+                );
+    if (v != NULL)
+      return v->s2;
   }
-  
+    
   return("text/plain");
 }
 
