b9df624875d4c3702096578da91ee593d198633c
Sean Conner <spc@conman.org>
2015-07-18 18:01:34 -0400

    Convert HTTP status codes into an enum.
    This has been bothering me for a while now, and since I'm here making
    hanges, why not this?

--- src/fix.h
+++ src/fix.h
@@ -33,71 +33,74 @@
 	; Informational Codes
 	;----------------------------------*/
 
-#define HTTP_CONTINUE		100		/* 1.1	*/
-#define HTTP_SWITCHPROTO	101		/* 1.1	*/
-#define HTTP_100MAX		101
+typedef enum http__e
+{
+  HTTP_CONTINUE		= 100,		/* 1.1	*/
+  HTTP_SWITCHPROTO	= 101,		/* 1.1	*/
+  HTTP_100MAX		= 101,
 
 	/*--------------------------------------
 	; Successful Codes
 	;--------------------------------------*/
 	
-#define HTTP_OKAY		200		/* 1.0	*/
-#define HTTP_CREATED		201		/* 1.0	*/
-#define HTTP_ACCEPTED		202		/* 1.0	*/
-#define HTTP_NONAUTH		203		/* 1.1	*/
-#define HTTP_NOCONTENT		204		/* 1.0	*/
-#define HTTP_RESETCONTENT	205		/* 1.1	*/
-#define HTTP_PARTIALCONTENT	206		/* 1.1	*/
-#define HTTP_200MAX		206
+  HTTP_OKAY		= 200,		/* 1.0	*/
+  HTTP_CREATED		= 201,		/* 1.0	*/
+  HTTP_ACCEPTED		= 202,		/* 1.0	*/
+  HTTP_NONAUTH		= 203,		/* 1.1	*/
+  HTTP_NOCONTENT	= 204,		/* 1.0	*/
+  HTTP_RESETCONTENT	= 205,		/* 1.1	*/
+  HTTP_PARTIALCONTENT	= 206,		/* 1.1	*/
+  HTTP_200MAX		= 206,
 
 	/*----------------------------------------
 	; Redirection Codes
 	;----------------------------------------*/
 
-#define HTTP_300		300
-#define HTTP_MULTICHOICE	300		/* 1.1	*/
-#define HTTP_MOVEPERM		301		/* 1.0	*/
-#define HTTP_MOVETEMP		302		/* 1.0	*/
-#define HTTP_SEEOTHER		303		/* 1.1	*/
-#define HTTP_NOTMODIFIED	304		/* 1.0	*/
-#define HTTP_USEPROXY		305		/* 1.1	*/
-#define HTTP_300MAX		305
+  HTTP_300		= 300,
+  HTTP_MULTICHOICE	= 300,		/* 1.1	*/
+  HTTP_MOVEPERM		= 301,		/* 1.0	*/
+  HTTP_MOVETEMP		= 302,		/* 1.0	*/
+  HTTP_SEEOTHER		= 303,		/* 1.1	*/
+  HTTP_NOTMODIFIED	= 304,		/* 1.0	*/
+  HTTP_USEPROXY		= 305,		/* 1.1	*/
+  HTTP_300MAX		= 305,
 
 	/*-----------------------------------------
 	; Client Error
 	;----------------------------------------*/
 	
-#define HTTP_BADREQ		400		/* 1.0	*/
-#define HTTP_UNAUTHORIZED	401		/* 1.0	*/
-#define HTTP_PAYMENTREQ		402		/* 1.1	*/
-#define HTTP_FORBIDDEN		403		/* 1.0	*/
-#define HTTP_NOTFOUND		404		/* 1.0	*/
-#define HTTP_METHODNOTALLOWED	405		/* 1.1	*/
-#define HTTP_NOTACCEPTABLE	406		/* 1.1	*/
-#define HTTP_PROXYAUTHREQ	407		/* 1.1	*/
-#define HTTP_TIMEOUT		408		/* 1.1	*/
-#define HTTP_CONFLICT		409		/* 1.1	*/
-#define HTTP_GONE		410		/* 1.1	*/
-#define HTTP_LENGTHREQ		411		/* 1.1	*/
-#define HTTP_PRECONDITION	412		/* 1.1	*/
-#define HTTP_TOOLARGE		413		/* 1.1	*/
-#define HTTP_URITOOLONG		414		/* 1.1	*/
-#define HTTP_MEDIATYPE		415		/* 1.1	*/
-#define HTTP_RANGEERROR		416		/* 1.1	*/
-#define HTTP_EXPECTATION	417		/* 1.1	*/
-#define HTTP_400MAX		417
+  HTTP_BADREQ		= 400,		/* 1.0	*/
+  HTTP_UNAUTHORIZED	= 401,		/* 1.0	*/
+  HTTP_PAYMENTREQ	= 402,		/* 1.1	*/
+  HTTP_FORBIDDEN	= 403,		/* 1.0	*/
+  HTTP_NOTFOUND		= 404,		/* 1.0	*/
+  HTTP_METHODNOTALLOWED	= 405,		/* 1.1	*/
+  HTTP_NOTACCEPTABLE	= 406,		/* 1.1	*/
+  HTTP_PROXYAUTHREQ	= 407,		/* 1.1	*/
+  HTTP_TIMEOUT		= 408,		/* 1.1	*/
+  HTTP_CONFLICT		= 409,		/* 1.1	*/
+  HTTP_GONE		= 410,		/* 1.1	*/
+  HTTP_LENGTHREQ	= 411,		/* 1.1	*/
+  HTTP_PRECONDITION	= 412,		/* 1.1	*/
+  HTTP_TOOLARGE		= 413,		/* 1.1	*/
+  HTTP_URITOOLONG	= 414,		/* 1.1	*/
+  HTTP_MEDIATYPE	= 415,		/* 1.1	*/
+  HTTP_RANGEERROR	= 416,		/* 1.1	*/
+  HTTP_EXPECTATION	= 417,		/* 1.1	*/
+  HTTP_400MAX		= 417,
 
 	/*-----------------------------------------
 	; Server Errors
 	;-----------------------------------------*/
 	
-#define HTTP_ISERVERERR		500		/* 1.0	*/
-#define HTTP_NOTIMP		501		/* 1.0	*/
-#define HTTP_BADGATEWAY		502		/* 1.0	*/
-#define HTTP_NOSERVICE		503		/* 1.0	*/
-#define HTTP_GATEWAYTIMEOUT	504		/* 1.1	*/
-#define HTTP_HTTPVERSION	505		/* 1.1	*/
-#define HTTP_500MAX		505
+  HTTP_ISERVERERR	= 500,		/* 1.0	*/
+  HTTP_NOTIMP		= 501,		/* 1.0	*/
+  HTTP_BADGATEWAY	= 502,		/* 1.0	*/
+  HTTP_NOSERVICE	= 503,		/* 1.0	*/
+  HTTP_GATEWAYTIMEOUT	= 504,		/* 1.1	*/
+  HTTP_HTTPVERSION 	= 505,		/* 1.1	*/
+  HTTP_500MAX		= 505,
+} http__e;
 
 extern char	*get_remote_user		(void);
 extern bool	 authenticate_author		(Request);
