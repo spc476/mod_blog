3a6121ede034503e907dbbe0bcd04131e3e5fa52
Sean Conner <spc@conman.org>
2017-10-07 21:20:37 -0400

    Fix comment.

--- src/main.c
+++ src/main.c
@@ -37,7 +37,7 @@ int main(int argc,char *argv[],char *envp[])
   crashreport_with(argc,argv,envp);
   
   /*---------------------
-  ; Defined in ANSI C
+  ; Defined by ANSI C
   ;-----------------------*/
   
   crashreport(SIGABRT);
