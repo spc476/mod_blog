fa27989034440e4baf5917a553f69a199c2a7af4
Sean Conner <spc@conman.org>
2026-05-21 22:46:02 -0400

    Bug fix---fix the PUT script to actually work with large files

--- Lua/put.lua
+++ Lua/put.lua
@@ -1,22 +1,31 @@
-#!/usr/bin/env lua
+#!/usr/bin/env Lua
+-- GPL2+ Copyright 2026 by Sean Conner
 -- luacheck: ignore 611
+-- ************************************************************************
+-- for PUT:
+--	201 Created
+--	202 Accepted		entry-post-hook failed
+--	400 Bad Request
+--	401 Unauthorized
+--	408 Timed out
+--	422 Unprocess Entiry	entry-pre-hook failed
+--	500 Internal Server Error
+-- ************************************************************************
 
-local dump    = require "org.conman.table".dump
-local tls     = require "org.conman.net.tls"
-local tcp     = require "org.conman.net.tcp"
-local url     = require "org.conman.parsers.url"
-local magic   = require "org.conman.fsys.magic"
-local fsys    = require "org.conman.fsys"
-local http    = require "org.conman.parsers.http-client"
-local getopt  = require "org.conman.getopt".getopt
-local ih      = require "org.conman.parsers.ih"
-local base64  = require "org.conman.base64"()
-local uurl    = require "url-util"
-local status  = require "org.conman.const.http-response"
-local zlib    = require "zlib"
+local tls         = require "org.conman.net.tls"
+local tcp         = require "org.conman.net.tcp"
+local url         = require "org.conman.parsers.url"
+local magic       = require "org.conman.fsys.magic"
+local fsys        = require "org.conman.fsys"
+local http        = require "org.conman.parsers.http-client"
+local getopt      = require "org.conman.getopt".getopt
+local ih          = require "org.conman.parsers.ih"
+local base64      = require "org.conman.base64"()
+local uurl        = require "url-util"
+local http_status = require "org.conman.const.http-response"
+local zlib        = require "zlib"
 
-local headers = ih.headers(ih.generic)
-local gf_debug
+local headers     = ih.headers(ih.generic)
 
 magic:flags('mime')
 
@@ -35,36 +44,7 @@ end
 
 -- ************************************************************************
 
-local function readbody(conn,hdrs)
-  local body
-  
-  if hdrs['Content-Length'] then
-    body = conn:read(hdrs['Content-Length'])
-  elseif hdrs['Transfer-Encoding'] == 'chunked' then
-    body = ""
-    repeat
-      local line = conn:read("l")
-      if not line then break end
-      local len  = tonumber(line,16)
-      if not len then break end
-      body       = body .. (conn:read(len) or "")
-      conn:read("l")
-    until len == 0
-  else
-    body = ""
-  end
-  
-  if hdrs['Content-Encoding'] == 'gzip' then
-    local s = zlib.inflate(body)
-    body    = s:read("*a")
-  end
-  
-  return body
-end
-
--- ***********************************************************************
-
-local function get(conn,location)
+local function request(conn,method,location,closef,user,mimetype,body)
   local path do
     if location.query then
       path = string.format("%s?%s",location.path,location.query)
@@ -74,72 +54,66 @@ local function get(conn,location)
   end
   
   conn:write(
-          string.format("GET %s HTTP/1.1\r\n",path),
+          string.format("%s %s HTTP/1.1\r\n",method,path),
           string.format("Host: %s:%d\r\n",location.host,location.port),
-          "User-Agent: mod-blog-updater/1.0\r\n",
-          "Accept: text/plain\r\n",
-          "\r\n"
-  );
-  conn:flush()
-  
-  local response = conn:read("l")
-  local hdrs     = conn:read("h")
-  
-  response   = http.response:match(response)
-  hdrs       = http.headers:match(hdrs)
-  local body = readbody(conn,hdrs)
-  
-  if gf_debug then
-    dump("GET response",response)
-    dump("GET headers",hdrs)
-    dump("GET body",body)
-  end
-  
-  if response.status == status.SUCCESS.OKAY then
-    return body,response.status
-  else
-    return nil,response.status
-  end
-end
-
--- ***********************************************************************
--- 201 Created
--- 202 Accepted           (entry-post-hook failed)
--- 408 Timed out          (entry-post-hook failed)
--- 422 Unprocessed Entity (entry-pre-hook failed)
--- ***********************************************************************
-
-local function put(conn,location,hdrs,mimetype,body)
-  conn:write(
-          string.format("PUT %s HTTP/1.1\r\n",location.path),
-          string.format("Host: %s:%d\r\n",location.host,location.port),
-          "User-Agent: mod-blog-updater/1.0\r\n",
-          string.format("Content-Type: %s\r\n",mimetype),
-          string.format("Content-Length: %d\r\n",#body),
+          "User-Agent: mod-blog-updater/1.2\r\n",
           "Accept: */*\r\n"
   )
   
-  for name,val in pairs(hdrs) do
-    conn:write(string.format("%s: %s\r\n",name,val))
+  if mimetype then
+    conn:write(
+            string.format("Content-Type: %s\r\n",mimetype),
+            string.format("Content-Length: %d\r\n",#body)
+    )
   end
   
-  conn:write("\r\n",body)
+  if closef then
+    conn:write("Connection: close\r\n")
+  end
+  
+  if user then
+    conn:write(string.format("Authorization: %s\r\n",user))
+  end
+  
+  conn:write("\r\n")
   conn:flush()
   
-  local r = conn:read("l")
-  local h = conn:read("h")
+  if body then
+    while #body > 0 do
+      local data = body:sub(1,1024)
+      conn:write(data)
+      conn:flush()
+      body = body:sub(1025,-1)
+    end
+  end
+    
+  local r   = conn:read("l")
+  local h   = conn:read("h")
+  local res = http.response:match(r)
+  local hdr = http.headers:match(h)
   
-  local res  = http.response:match(r)
-  local hdr  = http.headers:match(h)
-  body       = readbody(conn,hdr)
-  
-  if gf_debug then
-    dump("PUT response",res)
-    dump("PUT headers",hdr)
-    dump("PUT body",body)
+  if hdr['Content-Length'] then
+    body = conn:read(hdr['Content-Length'])
+  elseif hdr['Transfer-Encoding'] == 'chunked' then
+    body = ""
+    repeat
+      local line = conn:read("l")
+      if not line then break end
+      local len = tonumber(line,16)
+      if not len then break end
+      body = body .. (conn:read(len) or "")
+      conn:read("l")
+    until len == 0
+  else
+    body = ""
   end
   
-  return res.status >= 200 and res.status <= 299 , res.status
+  if hdr['Content-Encoding'] == 'gzip' then
+    local s = zlib.inflate(body)
+    body    = s:read("*a")
+  end
+  
+  return res.status,body
 end
 
 -- ************************************************************************
@@ -162,7 +136,7 @@ local function loadentry(filename)
   local hdrs = headers:match(body)
   
   if not hdrs then
-    errmsg("%s: not a proper entry")
+    errmsg("%s: not a proper entry",filename)
     return
   end
   
@@ -178,9 +152,8 @@ end
 local function usage()
   io.stderr:write(string.format([[
 usage: %s [options] entryfile [files...]
-        -b | --blog blogref
-        -d | --debug
-        -h | --help
+	-b | --blog blogref
+	-h | --help
 ]],arg[0]))
   os.exit(1,true)
 end
@@ -191,12 +164,10 @@ local CONF = "/home/spc/.config/mod-blog-put.config"
 local BLOG = 'default'
 local BASEURL
 local USER
-
 local opts =
 {
-  { "b" , "blog"  , true  , function(b) BLOG = b        end },
-  { "d" , "debug" , false , function()  gf_debug = true end },
-  { "h" , "help"  , false , function()  usage()         end },
+  { "b" , "blog"  , true  , function(b) BLOG = b end },
+  { "h" , "help"  , false , function()  usage()  end },
 }
 
 local fi = getopt(arg,opts)
@@ -211,33 +182,26 @@ do
   if not conf then
     os.exit(errmsg("%s: %s",CONF,err),true)
   end
-  
+
   conf()
-  
+
   if not confenv[BLOG] then
-    os.exit(errmsg("%s: missing %q config block",CONF,BLOG))
+    os.exit(errmsg("%s: missing %q config block",CONF,BLOG),true)
   end
-  
+
   BASEURL = confenv[BLOG].url
-  
+
   if not BASEURL then
-    os.exit(errmsg("%s: missing %s.url directive",CONF,BLOG))
+    os.exit(errmsg("%s: missing %s.url directive",CONF,BLOG),true)
   end
-  
+
   USER = confenv[BLOG].user
   if USER then
     USER = "Basic " .. base64:encode(USER)
   end
 end
 
--- ------------------------------------------------------------------
-
-if gf_debug then
-  io.stderr:write(string.format([[
-base-url=%q
-entry-file=%q
-]],BASEURL,arg[fi]))
-end
+-- ************************************************************************
 
 local body,date = loadentry(arg[fi])
 
@@ -249,10 +213,6 @@ local base     = url:match(BASEURL)
 local path     = url:match(date .. '/' .. fsys.basename(arg[fi]))
 local location = uurl.merge(base,path)
 
-if gf_debug then
-  io.stderr:write(string.format("location: %s body: %d\n",uurl.toa(location),#body))
-end
-
 local conn,err do
   if location.scheme == 'https' then
     conn,err = tls.connect(location.host,location.port)
@@ -262,85 +222,67 @@ local conn,err do
 end
 
 if not conn then
-  os.exit(errmsg("%s: %s",location.host,err))
+  os.exit(errmsg("%s: %s",location.host,err),true)
 end
+
+-- --------------------------------
+-- Get next entry URL
+-- --------------------------------
 
 local newurl do
+  local status,bodyg
   local basen = url:match(BASEURL)
   local pathn = url:match("/boston.cgi?cmd=last&date="..date)
-  local locationn = uurl.merge(basen,pathn)
-  newurl = get(conn,locationn)
+  location    = uurl.merge(basen,pathn)
   
-  local front,part = newurl:match("^(.*)(%d+)$")
-  part = tonumber(part) + 1
+  msg("GET '%s'",uurl.toa(location))
+  status,bodyg = request(conn,"GET",location)
+  
+  if status ~= http_status.SUCCESS.OKAY then
+    os.exit(errmsg("%d `%s`",status,uurl.toa(location)),true)
+  end
+  
+  local front,part = bodyg:match("^(.*)(%d+)$")
+  part   = tonumber(part) + 1
   newurl = front .. part
-end
-
-if gf_debug then
-  io.stderr:write(string.format("newurl=%q\n",newurl))
 end
 
 location = url:match(newurl)
 
-local http_headers = {} do
-  if USER then
-    http_headers['Authorization'] = USER
-  end
-  
-  if fi == #arg then
-    http_headers['Connection'] = 'close'
-  end
-  
-  if gf_debug then
-    dump("headers",http_headers)
-  end
+-- ------------------------------------------
+-- PUT the new entry
+-- ------------------------------------------
+
+msg("PUT '%s' (%d)",uurl.toa(location),#body)
+local status = request(conn,"PUT",location,fi == #arg,USER,"application/mod_blog",body)
+if status ~= http_status.SUCCESS.CREATED then
+  os.exit(errmsg("PUT: %d '%s'",status,uurl.toa(location)),true)
 end
 
-msg("PUT %s (%d)",uurl.toa(location),#body)
-if not put(conn,location,http_headers,"application/mod_blog",body) then
-  conn:close()
-  errmsg("Failed: PUT %s",uurl.toa(location))
-  errmsg("\tForgot credentials?")
-  os.exit(1,true)
-end
+-- -----------------------------------
+-- PUT any other files specified
+-- -----------------------------------
+
+local excode = 0
 
 for i = fi + 1 , #arg do
   local f,err2 = io.open(arg[i],"rb")
   if f then
-    local bodyf  = f:read("*a")
+    body = f:read("*a")
     f:close()
     
     local pathf     = url:match(date .. '/' .. fsys.basename(arg[i]))
     local locationf = uurl.merge(base,pathf)
     
-    if gf_debug then
-      io.stderr:write(string.format(
-        "location: %s body: %d mime: %s\n",
-        uurl.toa(locationf),
-        #bodyf,
-        magic(bodyf,true)
-      ))
+    msg("PUT '%s' (%d)",uurl.toa(locationf),#body)
+    status = request(conn,"PUT",locationf,i==#arg,USER,magic(body,true),body)
+    
+    if status ~= http_status.SUCCESS.CREATED then
+      excode = errmsg("PUT: %d '%s'",status,uurl.toa(locationf))
     end
-    
-    local http_headersf = { ['Blog-File'] = true }
-    
-    if USER then
-      http_headersf['Authorization'] = USER
-    end
-    
-    if i == #arg then
-      http_headersf['Connection'] = 'close'
-    end
-    
-    if gf_debug then
-      dump("headers",http_headersf)
-    end
-    
-    msg("PUT %s (%d)",uurl.toa(locationf),#bodyf)
-    put(conn,locationf,http_headersf,magic(bodyf,true),bodyf)
   else
-    errmsg("%s: %s",arg[i],err2)
+    excode = errmsg("%s: %s",arg[i],err2)
   end
 end
 
-conn:close()
+os.exit(excode,true)
